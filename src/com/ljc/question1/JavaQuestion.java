08bab834367273e79e522a4f874e71a0031a83a3
林吉聪 <932997844@qq.com>
2016-08-02 11:33:51 +0800

    first

--- src/com/ljc/question1/JavaQuestion.java
+++ src/com/ljc/question1/JavaQuestion.java
@@ -1,10 +1,19 @@
 package com.ljc.question1;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class JavaQuestion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(fun(10));
+	}
+	
+	public static int fun(int n) {
+		if(n==1||n==2)
+			return 1;
+		else 
+			return fun(n-1)+fun(n-2);
+		}
 	}
 
-}
